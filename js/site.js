d431492dda5e8f2d03002244a43e6c1fe47ed074
Sean Klei <stk1151@rit.edu>
2018-03-30 20:33:21 -0400

    having it read the RSS Feed from espon

--- js/site.js
+++ js/site.js
@@ -1,10 +1,38 @@
 window.addEventListener('load', function() {
 	
-	/**************************
-	 * Interaction Handlers
-	 **************************/
+	loadRSSFeed("http://www.espon.com/espn/rss/HTML/news");
 	
+	function loadRSSFeed(url) {
+		document.getElementById("rss-reader").innerText = "Loading RSS Feed";
+		$.get({
+			url: url,
+			success: function(data) {
+				loadXML(data);
+			}
+		});
+	}	
 	
+	function loadXML(obj) {
+		var items = obj.querySelectorAll("item");
+		var html = "";
+		for (var i = 0; i < items.length; i++) {
+			var newsItem = items[i];
+			var title = newsItem.querySelector("title").firstChild.nodeValue;
+			var description = newsItem.querySelector("description").firstChild.nodeValue;
+			var link = newsItem.querySelector("link").firstChild.nodeValue;
+			var pubDate = newsItem.querySelector("pubDate").firstChild.nodeValue;
+			var line = '<div class="item">';
+			line += "<h2>"+title+"</h2>";
+			line += '<p><i>'+pubDate+'</i> - <a href="'+link+'" target="_blank">See original</a></p>';
+			//title and description are always the same (for some reason) so I'm only including one
+			//line += "<p>"+description+"</p>";
+			line += "</div>";
+			
+			html += line;
+		}
+		document.getElementById("rss-reader").innerHTML = html;	
+	}
+
 });
 
 function loadTextButtonOnClick(event) {
@@ -47,3 +75,4 @@ function loadJson(url) {
 		}
 	});
 }
+
