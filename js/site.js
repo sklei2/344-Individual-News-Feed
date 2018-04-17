7d8272343affc5723ac7ded52fdf96d1b8aee4e3
sklei2 <stk1151@rit.edu>
2018-04-17 13:35:11 -0400

    making it so we load all the RSS feeds

--- js/site.js
+++ js/site.js
@@ -11,6 +11,7 @@ class newsItem {
 
 window.addEventListener('load', function() {
 
+	// enum variable for our sports
 	var sport_enum = Object.freeze({
 		MLB: 1,
 		NBA: 2,
@@ -18,28 +19,47 @@ window.addEventListener('load', function() {
 		NHL: 4,
 	});
 	
+	// Setup our initial data
 	var newsFeed = [];
 	var espnUrl = "http://www.espn.com/espn/rss/";
 	var afterSportUrl = "/news"
 
 	var checkboxes = {
-		mlbCheckbox: document.getElementById("mlb-checkbox"),
-		nbaCheckbox: document.getElementById("nba-checkbox"),
-		nflCheckbox: document.getElementById("nfl-checkbox"),
-		nhlCheckbox: document.getElementById("nhl-checkbox"),
+		MLB: document.getElementById("mlb-checkbox"),
+		NBA: document.getElementById("nba-checkbox"),
+		NFL: document.getElementById("nfl-checkbox"),
+		NHL: document.getElementById("nhl-checkbox"),
+	}
+
+	// Setup the initial data to read all the RSS feeds
+	var finishedSports = {};
+
+	function readRSSCallback(sport) {
+		finishedSports[sport] = 1;
+		if (Object.keys(finishedSports).length >= Object.keys(sport_enum).length) {
+			loadNewsFeed();
+		}
 	}
 
 	for (key in checkboxes) {
-		checkboxes[key].addEventListener("change", filterChangeHandler);
+		var checkbox = checkboxes[key];
+		checkbox.addEventListener("change", filterChangeHandler);
+		checkbox.checked = true;
+
+		loadRSSFeed(espnUrl + key + afterSportUrl, sport_enum[key], readRSSCallback)
 	}
 	
-	function loadRSSFeed(url, sport) {
+	// RSS Handlers
+
+	function loadRSSFeed(url, sport, callback) {
 		document.getElementById("rss-reader").innerText = "Loading RSS Feed";
 		$.get({
 			url: url,
 			success: function(data) {
 				loadRSSData(data, sport);
-				loadNewsFeed();
+				if (callback) {
+					callback(sport);
+				}
 			}
 		});
 	}
@@ -58,6 +78,24 @@ window.addEventListener('load', function() {
 
 	function addNewsItem(item) {
 		newsFeed.push(item);
+	}
+
+	// Drawing HTML
+
+	function appendNewsItems(items) {
+		var html = document.getElementById("rss-reader").innerHTML
+		for (var i = 0; i < items.length; i++) {
+			var item = items[i];
+			var line = '<div class="item">';
+			line += "<h2>" + item.title + "</h2>";
+			line += '<p><i>'+ item.pubDate +'</i> - <a href="'+ item.link + '" target="_blank">See original</a></p>';
+			//title and description are always the same (for some reason) so I'm only including one
+			//line += "<p>"+description+"</p>";
+			line += "</div>";
+			
+			html += line;
+		}
+		document.getElementById("rss-reader").innerHTML = html;
 	}
 
 	function loadNewsFeed() {
